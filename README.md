42f479a9ea9976c5d19094fc53890c9a08f6baaa
Felipe Santos <lipe.fssantos063@gmail.com>
2020-06-10 15:22:09 -0300

    ..

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Modelador-de-Grafos
 Modela Grafos  em Python usando NetworkX
+
+
+![Screenshot](g.png)
--- a.py
+++ a.py
@@ -5,7 +5,7 @@ grafo = nx.DiGraph()
 
 nos = range(1,4)
 
-arestas = [(1,2), (2,3), (3,4), (4,2)]
+arestas = [[1,2], [2,3], [3,4], [4,2]]
 
 grafo.add_nodes_from(nos)
 grafo.add_edges_from(arestas)
